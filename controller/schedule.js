d7c0c747cf697f895f5cbe8617160f2cad8058de
Sunny955 <sunnyranjan9050@gmail.com>
2024-02-23 21:44:15 +0530

    Changed time zone to india

--- controller/schedule.js
+++ controller/schedule.js
@@ -5,22 +5,29 @@ const db = require("../config/dbConfig");
 db.query = util.promisify(db.query);
 
 const scheduleMorningForecast = (client) => {
-  cron.schedule("0 7 * * *", async () => {
-    try {
-      const query = "SELECT id FROM user WHERE location IS NOT NULL";
-      const output = await db.query(query);
+  cron.schedule(
+    "0 7 * * *",
+    async () => {
+      try {
+        const query = "SELECT id FROM user WHERE location IS NOT NULL";
+        const output = await db.query(query);
 
-      for (const row of output) {
-        const { id } = row;
-        const output = await sendMorningForecast({ id });
-        const user = await client.users.fetch(id);
-        user.send(output);
+        for (const row of output) {
+          const { id } = row;
+          const output = await sendMorningForecast({ id });
+          const user = await client.users.fetch(id);
+          user.send(output);
+        }
+
+        console.log("Morning forecast sent to all users successfully");
+      } catch (error) {
+        console.error("Error sending morning forecast:", error);
       }
-
-      console.log("Morning forecast sent to all users successfully");
-    } catch (error) {
-      console.error("Error sending morning forecast:", error);
+    },
+    {
+      scheduled: true,
+      timezone: "Etc/UTC",
     }
-  });
+  );
 };
 module.exports = { scheduleMorningForecast };
